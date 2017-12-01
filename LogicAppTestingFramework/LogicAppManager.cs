f19f04db75d835b7f6e9aacacbb880fccfb9da95
Shankar Chandrasekar <shankar.chandrasekar@inciper.com>
2017-12-01 10:06:16 +0000

    Added comments

--- LogicAppTestingFramework/LogicAppManager.cs
+++ LogicAppTestingFramework/LogicAppManager.cs
@@ -34,7 +34,9 @@ namespace LogicApp.AcceptanceTests.Utilities.LogicAppDiagnostics
             Other
         }
 
-
+        /// <summary>
+        /// Initializes the LogicManagementClient and assigns it to a static variable. This will be used the test framework to retrieve data using Azure Logic Apps SDK
+        /// </summary>
         public static void Start()
         {
             ServiceClientCredentials credentials = GetAzureCredentials();
@@ -42,17 +44,26 @@ namespace LogicApp.AcceptanceTests.Utilities.LogicAppDiagnostics
             _client.SubscriptionId = Config.SubscriptionId;
         }
 
+        /// <summary>
+        /// When a test completes, this method should be invoked to dispose the instance of the LogicManagementClient
+        /// </summary>
         public static void Stop()
         {
             _client.Dispose();
         }
 
+     
         public static void Clear()
         {
             _runId = null;
         }
 
-
+        /// <summary>
+        /// Check if the logic app is enabled
+        /// </summary>
+        /// <param name="resourceGroupName">Pass the Resource Group Name</param>
+        /// <param name="logicAppName">Pass the Logic App Name</param>
+        /// <returns></returns>
         public static bool CheckIfLogicAppIsEnabled(string resourceGroupName, string logicAppName)
         {
             bool enabled = false;
@@ -76,6 +87,13 @@ namespace LogicApp.AcceptanceTests.Utilities.LogicAppDiagnostics
             return enabled;
         }
 
+        /// <summary>
+        /// Execute a logic app. This method can be used to invoke logic apps which has a callback url and logic apps which has any other kind of trigger like servicebus trigger.
+        /// </summary>
+        /// <param name="type">Set either HTTP or other</param>
+        /// <param name="triggerName">The triggername that you specified in the logic app</param>
+        /// <param name="content">Message payload</param>
+        /// <returns></returns>
         public async static Task<string> ExecuteLogicApp(TriggerType type, string triggerName, HttpContent content)
         {
             if(type == TriggerType.Http)
@@ -110,12 +128,22 @@ namespace LogicApp.AcceptanceTests.Utilities.LogicAppDiagnostics
             }
         }
 
+        /// <summary>
+        /// Get the callbackurl for the Logic App
+        /// </summary>
+        /// <param name="triggerName"></param>
+        /// <returns></returns>
         private async static Task<AzureOperationResponse<WorkflowTriggerCallbackUrl>> GetCallBackUrl(string triggerName)
         {
             return await _client.WorkflowTriggers.
                 ListCallbackUrlWithHttpMessagesAsync(_resourceGroupName, _logicAppName, triggerName);
         }
 
+        /// <summary>
+        /// Execute a logic app which is triggered by other compenents like SFTP, Azure service bus etc. 
+        /// </summary>
+        /// <param name="triggerName"></param>
+        /// <returns></returns>
         private async static Task<string> ExecuteLogicAppWithOtherTriggerType(string triggerName)
         {
             var result = await _client.WorkflowTriggers.
@@ -123,6 +151,11 @@ namespace LogicApp.AcceptanceTests.Utilities.LogicAppDiagnostics
             return result.ToString();
         }
 
+        /// <summary>
+        /// Check the status of any action of  a completed logic app by passing the run identifier
+        /// </summary>
+        /// <param name="actionName"></param>
+        /// <returns></returns>
         public async static Task<string> CheckLogicAppAction(string actionName)
         {
             var r = await _client.WorkflowRunActions.ListAsync(_resourceGroupName, _logicAppName, _runId);
@@ -132,6 +165,10 @@ namespace LogicApp.AcceptanceTests.Utilities.LogicAppDiagnostics
                 .First().ToString();
         }
 
+        /// <summary>
+        /// Pass the SPN and azure subscription details and generate a OAUTH2.0 token
+        /// </summary>
+        /// <returns></returns>
         private static ServiceClientCredentials GetAzureCredentials()
         {
             var authContext = new AuthenticationContext(string.Format("https://login.windows.net/{0}", Config.TenantId));
